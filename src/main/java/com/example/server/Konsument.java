8e63e15e381fef861561cc9a40ffd40c9bf0e24a
Mikolaj <xxmikskixx@gmail.com>
2022-04-23 21:28:35 +0200

    Running Server :)

--- src/main/java/com/example/server/Konsument.java
+++ src/main/java/com/example/server/Konsument.java
@@ -1,5 +1,6 @@
 package com.example.server;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -9,7 +10,10 @@ public class Konsument implements Runnable {
     BlockingQueue<Odpowiedz> kolejka;
     Controller controller;
 
-    Map<String, String> pytanieOdpowiedz = preparePytanieOdpowiedzMap();
+
+    String[] subs;
+
+//    Map<String, String> pytanieOdpowiedz = preparePytanieOdpowiedzMap();
 
     public Konsument(BlockingQueue<Odpowiedz> kolejka, Controller controller) {
         this.kolejka = kolejka;
@@ -20,17 +24,24 @@ public class Konsument implements Runnable {
     @Override
     public void run() {
         try {
-            Set<String> pytania = pytanieOdpowiedz.keySet();
-            for (String pytanie : pytania) {
-                controller.appendQuestion(pytanie);
-
+//            Set<String> pytania = pytanieOdpowiedz.keySet();
+            String s = readFile();
+            String [] subs = s.split(",");
+            int k = 1;
+            int j = 0;
+            for (int i=0;i<subs.length-2;i++) {
+                controller.appendQuestion(subs[j]);
                 while (true){
                     Odpowiedz odpowiedz = kolejka.take();
                     String tresc = odpowiedz.getTresc();
                     String ipAddress = odpowiedz.getIpAddress();
                     if(tresc != null) {
                         String[] split = tresc.split(":");
-                        if (split[1].equals(pytanieOdpowiedz.get(pytanie))) {
+                        if (split[1].equals(subs[k])){
+                            if(k+2<= subs.length && j+2<= subs.length) {
+                                j = j + 2;
+                                k = k + 2;
+                            }
                             controller.appendOdpowiedzPoprawna(split[0], ipAddress);
                             break;
                         } else {
@@ -40,15 +51,31 @@ public class Konsument implements Runnable {
                 }
             }
             controller.appendFinalMessage();
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    Map<String, String> preparePytanieOdpowiedzMap() {
-        Map<String, String> result = new HashMap<>();
-        result.put("Stolica Polski?", "Warszawa");
-        result.put("Najlepszy jezyk programowania?", "Kotlin");
-        return result;
+//    Map<String, String> preparePytanieOdpowiedzMap() {
+//        Map<String, String> result = new HashMap<>();
+//        result.put("Stolica Polski?", "Warszawa");
+//        result.put("Najlepszy jezyk programowania?", "Kotlin");
+//        return result;
+//    }
+
+    public String readFile() throws IOException {
+        // Using the Java 7 "try with resource syntax".
+        try (FileReader fr = new FileReader("src/main/java/com/example/server/pytania.txt")) {
+            BufferedReader br = new BufferedReader(fr);
+            StringBuilder content = new StringBuilder();
+            int c;
+            while ((c = br.read()) != -1) {
+                content.append((char)c);
+            }
+            return content.toString();
+        }
     }
+
 }
+
+
